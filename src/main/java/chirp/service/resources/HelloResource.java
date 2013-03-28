f4bd56c2445c0f9ff59bb5334a82d8acf6e0adc8
Dan Rosen <dr@mergeconflict.com>
2013-03-28 12:55:31 -0700

    FormParam

--- src/main/java/chirp/service/resources/HelloResource.java
+++ src/main/java/chirp/service/resources/HelloResource.java
@@ -1,6 +1,8 @@
 package chirp.service.resources;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
@@ -16,6 +18,11 @@ public class HelloResource {
 	@GET
 	@Path("{name}")
 	public String getHelloSubresource(@PathParam("name") String name) {
+		return String.format("Hello, %s!", name);
+	}
+
+	@POST
+	public String postHello(@FormParam("name") String name) {
 		return String.format("Hello, %s!", name);
 	}
 
