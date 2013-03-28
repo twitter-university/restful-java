cbc581878a53d5d31bbfaa404b9f6385ef9dca9b
Dan Rosen <dr@mergeconflict.com>
2013-03-28 12:50:04 -0700

    PathParam

--- src/main/java/chirp/service/resources/HelloResource.java
+++ src/main/java/chirp/service/resources/HelloResource.java
@@ -2,6 +2,7 @@ package chirp.service.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 
 @Path("hello")
@@ -9,6 +10,12 @@ public class HelloResource {
 
 	@GET
 	public String getHello(@QueryParam("name") String name) {
+		return String.format("Hello, %s!", name);
+	}
+
+	@GET
+	@Path("{name}")
+	public String getHelloSubresource(@PathParam("name") String name) {
 		return String.format("Hello, %s!", name);
 	}
 
--- src/test/java/chirp/service/resources/HelloResourceTest.java
+++ src/test/java/chirp/service/resources/HelloResourceTest.java
@@ -4,11 +4,21 @@ import static junit.framework.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.sun.jersey.api.client.WebResource;
+
 public class HelloResourceTest extends ResourceTest {
+
+	private final WebResource helloResource = resource().path("hello");
 
 	@Test
 	public void helloResourceMustSayHello() {
-		String hello = resource().path("hello").queryParam("name", "REST").get(String.class);
+		String hello = helloResource.queryParam("name", "REST").get(String.class);
+		assertEquals("Hello, REST!", hello);
+	}
+
+	@Test
+	public void helloSubresourceMustSayHello() {
+		String hello = helloResource.path("REST").get(String.class);
 		assertEquals("Hello, REST!", hello);
 	}
 
