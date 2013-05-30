8291ccd08f1ab421637cbfe316628344dbbc3e64
Gordon Force <gordon@force-associates.com>
2013-05-30 11:34:38 -0700

    Added support for a post.user link and user.posts links.

--- src/main/java/chirp/service/representations/PostRepresentation.java
+++ src/main/java/chirp/service/representations/PostRepresentation.java
@@ -9,27 +9,34 @@ import org.codehaus.jackson.annotate.JsonProperty;
 
 import chirp.model.Post;
 import chirp.service.resources.PostResource;
+import chirp.service.resources.UsersResource;
 
 public class PostRepresentation {
 
 	private final URI self;
 	private final String content;
 	private final String timestamp;
+	private final URI user;
 
 	public PostRepresentation(Post post) {
+		String username = post.getUser().getUsername();
 		this.content = post.getContent();
 		this.timestamp = post.getTimestamp().toString();
 		this.self = UriBuilder.fromResource(PostResource.class)
-				.path(this.timestamp).build(post.getUser().getUsername());
+				.path(this.timestamp).build(username);
+		this.user = UriBuilder.fromResource(UsersResource.class).path(username)
+				.build();
 	}
 
 	@JsonCreator
 	public PostRepresentation(@JsonProperty("self") URI self,
 			@JsonProperty("content") String content,
-			@JsonProperty("timestamp") String timestamp) {
+			@JsonProperty("timestamp") String timestamp,
+			@JsonProperty("user") URI user) {
 		this.content = content;
 		this.timestamp = timestamp;
 		this.self = self;
+		this.user = user;
 	}
 
 	public String getContent() {
@@ -42,6 +49,10 @@ public class PostRepresentation {
 
 	public URI getSelf() {
 		return self;
+	}
+
+	public URI getUser() {
+		return user;
 	}
 
 }
--- src/main/java/chirp/service/representations/UserRepresentation.java
+++ src/main/java/chirp/service/representations/UserRepresentation.java
@@ -8,6 +8,7 @@ import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 import chirp.model.User;
+import chirp.service.resources.PostResource;
 import chirp.service.resources.UsersResource;
 
 public class UserRepresentation {
@@ -15,21 +16,25 @@ public class UserRepresentation {
 	private final URI self;
 	private final String username;
 	private final String realname;
+	private final URI posts;
 
 	public UserRepresentation(User user) {
 		this.username = user.getUsername();
 		this.realname = user.getRealname();
 		this.self = UriBuilder.fromResource(UsersResource.class).path(username)
 				.build();
+		this.posts = UriBuilder.fromResource(PostResource.class).build(username);
 	}
 
 	@JsonCreator
 	public UserRepresentation(@JsonProperty("self") URI self,
 			@JsonProperty("username") String username,
-			@JsonProperty("realname") String realname) {
+			@JsonProperty("realname") String realname,
+			@JsonProperty("posts") URI posts) {
 		this.self = self;
 		this.username = username;
 		this.realname = realname;
+		this.posts = posts;
 	}
 
 	public String getUsername() {
@@ -42,6 +47,10 @@ public class UserRepresentation {
 
 	public URI getSelf() {
 		return self;
+	}
+
+	public URI getPosts() {
+		return posts;
 	}
 
 }
