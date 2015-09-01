6e6e7c1ef4db78e38af06557046d8a32fdf79948
Jacob D. Parr <me@jacobparr.com>
2015-09-01 11:39:55 -0700

    HttpHeaders

--- src/main/java/com/example/chirp/app/resources/GreetingsResource.java
+++ src/main/java/com/example/chirp/app/resources/GreetingsResource.java
@@ -2,11 +2,12 @@ package com.example.chirp.app.resources;
 
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -23,20 +24,10 @@ public class GreetingsResource {
 	@Path("/{someName}")
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response sayHelloWithPathParam(@PathParam("someName") String name,
-			                                  @HeaderParam("X-NewCircle-Echo") String headerValue) {
+																				@Context HttpHeaders httpHeaders) {
 
-		// return "Hello "+name+"!";
-
-//		Response.ok()
-//		.entity(Entity.entity("Hello "+name+"!", MediaType.TEXT_PLAIN))
-//				.header("X-NewCircle-Echo-Response", headerValue)
-//				.build();
-//
-//		Response.status(200)
-//		.entity(Entity.entity("Hello "+name+"!", MediaType.TEXT_PLAIN))
-//				.header("X-NewCircle-Echo-Response", headerValue)
-//				.build();
-
+		String headerValue = httpHeaders.getHeaderString("X-NewCircle-Echo");
+		
 		return Response.ok("Hello "+name+"!")
 				.header("X-NewCircle-Echo-Response", headerValue)
 				.build();
